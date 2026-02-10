ca61441126f14f2bb99e8086a02fd81eedc24d7d
Iosif2510 <gpotatok@gmail.com>
2026-02-10 18:01:04 +0900

    Slider with number field

--- Runtime/Editor/InlineToggleDrawer.cs
+++ Runtime/Editor/InlineToggleDrawer.cs
@@ -11,8 +11,6 @@ namespace _2510.SimpleMeshOutline.Editor
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
             var attr = (InlineToggleAttribute)attribute;
-        
-            // 부모 경로를 통해 형제(bool) 프로퍼티를 찾습니다.
             string path = property.propertyPath;
             int lastDot = path.LastIndexOf('.');
             string parentPath = lastDot != -1 ? path.Substring(0, lastDot + 1) : "";
@@ -22,39 +20,39 @@ namespace _2510.SimpleMeshOutline.Editor
 
             if (toggleProp != null && toggleProp.propertyType == SerializedPropertyType.Boolean)
             {
-                // 1. 토글 생성 (라벨 없이)
+                // 1. 토글 생성
                 var toggleField = new PropertyField(toggleProp, "");
                 toggleField.style.width = 20;
                 toggleField.style.marginRight = 5;
 
-                // 2. 실제 데이터 필드 생성
+                // 2. 데이터 필드 생성 (Range 여부에 따라 분기)
                 VisualElement dataField;
                 if (attr.UseRange && property.propertyType == SerializedPropertyType.Float)
                 {
-                    dataField = new Slider(property.displayName, attr.Min, attr.Max) { bindingPath = property.propertyPath };
+                    // 슬라이더 생성 및 숫자 입력칸 활성화
+                    var slider = new Slider(property.displayName, attr.Min, attr.Max)
+                    {
+                        bindingPath = property.propertyPath,
+                        showInputField = true // 이 설정이 숫자 입력칸을 나타나게 합니다.
+                    };
+                    slider.labelElement.style.minWidth = 120; // 라벨 너비 확보
+                    dataField = slider;
                 }
                 else
                 {
                     dataField = new PropertyField(property, property.displayName);
                 }
+                
                 dataField.style.flexGrow = 1;
 
-                // 3. 활성화 상태 동기화 (초기값 및 변경 시)
+                // 3. 활성화 상태 동기화
                 void UpdateEnabled(bool val) => dataField.SetEnabled(val);
                 UpdateEnabled(toggleProp.boolValue);
-            
                 toggleField.RegisterValueChangeCallback(evt => UpdateEnabled(evt.changedProperty.boolValue));
 
                 container.Add(toggleField);
                 container.Add(dataField);
             }
-            else
-            {
-                // 토글을 못 찾았을 경우 경고 문구와 함께 기본 필드 출력
-                container.Add(new Label($"[Error: {attr.ToggleFieldName} Not Found] ") { style = { color = Color.red } });
-                container.Add(new PropertyField(property));
-            }
-
             return container;
         }
     }
